fa17807b09a2ec2bc425cf65dc67e1c8904d9134
amit15277 <48946259+amit15277@users.noreply.github.com>
2021-05-05 23:24:16 +0530

    Delete variables.tf

--- variables.tf
+++
@@ -1,8 +0,0 @@
-variable "instancetype" {
-  default     = "t2.small"
-  description = "Category of EC2 instance"
-}
-
-# variable "instanceclass" {
-#   description = "Category of RDS instance"
-# }
